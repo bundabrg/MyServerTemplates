d6c1cf26358440d80368e4bd376877669461c26f
bundabrg <brendan@grieve.com.au>
2020-10-14 18:04:29 +0800

    Initial Implementation of Template Repository
    Squashed commit of the following:
    
    commit 4dfea207b58038873aecbc3fe479ce9a4dbbbaab
    Author: bundabrg <brendan@grieve.com.au>
    Date:   Wed Oct 14 16:30:26 2020 +0800
    
        Implement Build Script
    
    commit dd4796b79bb8006f66eb1c7fa4099586e4f4b716
    Author: bundabrg <brendan@grieve.com.au>
    Date:   Tue Oct 13 18:44:11 2020 +0800
    
        Restructure Templates Better
    
        * New naming format to better support multiple versions
    
    commit 55c2522406b061f5c2f5dd9613d231ab45890b83
    Author: bundabrg <bundabrg@grieve.com.au>
    Date:   Tue Oct 13 15:53:43 2020 +0800
    
        Start structure

--- build.py
+++ build.py
@@ -1,1 +1,99 @@
-print("Hello World!")
+# MIT License
+#
+# Copyright (c) 2020 MyServer Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import gzip
+import hashlib
+import json
+import shutil
+import tarfile
+from pathlib import Path
+
+import yaml
+
+# Clear build folder
+build_path: Path = Path("build/templates")
+shutil.rmtree(build_path, ignore_errors=True)
+build_path.mkdir(parents=True, exist_ok=True)
+content_path = build_path / "contents"
+content_path.mkdir(parents=True, exist_ok=True)
+
+templates = {}
+
+# Find all template.yml files underneath the templates folder
+for template_file_path in Path("templates").rglob("template.yml"):
+    with open(template_file_path) as template_file:
+        template = yaml.full_load(template_file)
+
+        # Make sure template has a name
+        if "name" not in template:
+            print("Template missing name field: " + str(template_file_path))
+            continue
+
+        # Make sure its in the right format
+        fullname: str = template.get("name")
+        name_split = fullname.rsplit("@", 1)
+        if len(name_split) != 2:
+            print("Template has incorrect naming format: " + str(name[0]))
+            continue
+        name, version = name_split
+
+        print("Found: " + template.get("name") + " in " + str(template_file_path))
+
+        # Compress folder to base64 encoded file
+        temp_path = content_path / "~TMP.tgz"
+        with tarfile.open(temp_path, "w:gz") as compressed:
+            compressed.add(str(template_file_path.parent), arcname="")
+
+        # Calculate sha256
+        sha256_hash = hashlib.sha256()
+        with temp_path.open("rb") as compressed:
+            for byte_block in iter(lambda: compressed.read(4096), b""):
+                sha256_hash.update(byte_block)
+
+        # Rename file
+        temp_path.rename(content_path / (sha256_hash.hexdigest() + ".tgz"))
+
+        if name not in templates:
+            templates[name] = {}
+
+        templates[name][version] = sha256_hash.hexdigest()
+
+# Create contents.gz
+contents_file = build_path / "contents.gz"
+with gzip.open(contents_file, "w") as compressed:
+    compressed.write(str(json.dumps({
+        "version": 1.0,
+        "contents": templates,
+    })).encode())
+
+# Calculate sha256 of file
+sha256_hash = hashlib.sha256()
+with contents_file.open("rb") as compressed:
+    for byte_block in iter(lambda: compressed.read(4096), b""):
+        sha256_hash.update(byte_block)
+
+# Create release
+release_file = build_path / "release"
+with release_file.open("w") as output:
+    json.dump({
+        "version": 1.0,
+        "contents_sha": sha256_hash.hexdigest()
+    }, output)
